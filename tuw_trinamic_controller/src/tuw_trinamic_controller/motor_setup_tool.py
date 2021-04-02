6bd2f8febba42609e0323bda0ce61fb4dc0531e8
Eugen <eugen.kaltenegger@gmail.com>
2021-04-02 17:29:30 +0200

    fixed python import (pycharm: requires src folder to be marked as sources root)

--- tuw_trinamic_controller/src/tuw_trinamic_controller/motor_setup_tool.py
+++ tuw_trinamic_controller/src/tuw_trinamic_controller/motor_setup_tool.py
@@ -3,8 +3,8 @@
 from PyTrinamic.connections.ConnectionManager import ConnectionManager
 from PyTrinamic.modules.TMCM1640.TMCM_1640 import TMCM_1640
 
-from tuw_trinamic_controller.src.tuw_trinamic_controller.motor_configuration_tool import get_motor_configuration
-from tuw_trinamic_controller.src.tuw_trinamic_controller.motor_configuration_tool import set_motor_configuration
+from tuw_trinamic_controller.motor_configuration_tool import get_motor_configuration
+from tuw_trinamic_controller.motor_configuration_tool import set_motor_configuration
 
 
 def setup_motor_with_configuration(path_to_configuration_yaml):
